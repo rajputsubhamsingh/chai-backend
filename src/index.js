590aeccd28cb2c040cc5b9da049c441623737cb1
rajputsubhamsingh <skrsinghkrsingh.me2020@rceroorkee.in>
2024-01-29 16:01:19 +0530

    cors and cookie-parser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+        console.log("Error: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+    
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!!", err);
+  });
 
 
 
@@ -20,6 +34,22 @@ connectDB();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 /*
 import express from 'express'
 const app = express()
