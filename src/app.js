f5ed143b57ff5e49736dceaa60741c99b3418b8a
rajputsubhamsingh <skrsinghkrsingh.me2020@rceroorkee.in>
2024-02-20 12:02:56 +0530

    utils code discussion

--- src/app.js
+++ src/app.js
@@ -8,9 +8,11 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
-app.use(express.static("public"));
+app.use(express.json({limit: "16kb"}));                    
+app.use(express.urlencoded({extended: true, limit: "16kb"}));  // take the data from url 
+app.use(express.static("public"));          // store the file or pdf in public folder
+app.use(cookieParser());
+
 
 
 
--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
   } catch (error) {
     console.log("MONGODB connection FAILED: ", error);
     process.exit(1);
-  }
+  }  
 };
 
 export default connectDB;
