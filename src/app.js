96d56b6784b6c8cbecf9ca41ca477cd6a0ee6428
rajputsubhamsingh <skrsinghkrsingh.me2020@rceroorkee.in>
2024-02-21 17:36:57 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+
 const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,6 +15,16 @@ app.use(express.static("public"));          // store the file or pdf in public f
 app.use(cookieParser());
 
 
+// routes import
+import userRouter from './routes/user.routes.js';
 
+
+// routes declaration
+// app.use("/users", userRouter)
+// /users to thik hai par standard practice me hm usko versioning bhi dete hai 
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register 
 
 export { app };
