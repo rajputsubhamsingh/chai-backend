09a7baef2910c6e60922177f3b6c29fa230f6ac6
rajputsubhamsingh <skrsinghkrsingh.me2020@rceroorkee.in>
2024-02-22 12:27:37 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,86 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"; 
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js"; 
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    // res.status(200).json({
+    //     message: "ok"
+    // })
+
+    // register karne ke liye step 
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exits : username, email
+    // check for images, check for avatar 
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response
+
+    const {username, fullName, email, password} = req.body
+    // console.log(username, fullName, email, password);
+
+    // if condition laga ke sabke ke liye check kar skte hai 
+    // if(fullName === "") {
+    //     throw new ApiError(400, "fullName is required")
+    // }
+
+    // yaha bar bar if na laga ke ek sath hi sabke liye check kar rha hu.
+
+    if (
+        [username, fullName, email, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All feilds are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    });
+
+    if(existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar){
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    });
+    
+    // yaha jb user create ho jayega tb usko find kar rhe hai jis wajah pura data hme mil jayega but 
+    // hm chahte hai password na mile aur refreshToken to 
+    // uske liye hm select("-password -refreshToken") is method ka use kar rhe hai 
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 })
 
 export { registerUser };
